43efe78bda0c11415b0fd8fc2a836328531963b1
Eya Tlig <ayatlig07@gmail.com>
2023-10-15 13:06:21 +0200

    tp2

---
+++ app/src/main/java/com/example/eyatlig_mesure_de_glycemie/MainActivity.java
@@ -0,0 +1,15 @@
+ package com.example.eyatlig_mesure_de_glycemie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
