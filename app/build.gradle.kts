3c21d31d76fd438ea6c60a11435040e8d3aa0280
Eya Tlig <ayatlig07@gmail.com>
2023-11-09 16:14:01 +0100

    tp2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.eyatlig_mesure_de_glycemie"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.eyatlig_mesure_de_glycemie"
